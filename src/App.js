b1c57640f7e0ac55542aa7e7d6360bd15384c060
webdev_p <webdevp@email.com>
2023-02-11 17:26:16 -0500

    QR reader initial push

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {React} from 'react'
+import image from './image-qr-code.png'
+import {Helmet} from 'react-helmet'
+import './App.css';
+
+
+
+function App() {
+  return (
+    <>
+    <Helmet>
+    <meta charSet="utf-8" />
+    <title>My QR Reader</title>
+    <meta name="description" content="QR Reader Code Challenge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Helmet>
+    <div className='main'>
+      <div>
+      <img src={image} alt="qr-code" />
+      </div>
+      <section>
+       <div> 
+       <p className='mainbodyText'>Coding daily will help to improve your front-end skills</p>
+       </div>
+       <div>
+        <p className='subbodyText'>Scan the QR code to <br/> and start a coding challenge</p>
+        </div>
+      </section>
+    </div>
+    </>
+);
+}
+
+export default App;
