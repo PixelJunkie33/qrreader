14bd0679f95b0030df35f4e689136f0a6e65df5e
Scoubershare <10562319+Scoubershare@users.noreply.github.com>
2023-02-11 17:31:21 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import {React} from 'react'
-import image from './image-qr-code.png'
-import {Helmet} from 'react-helmet'
+import { React } from 'react';
+import image from './image-qr-code.png';
+import { Helmet } from 'react-helmet';
 import './App.css';
-
-
 
 function App() {
   return (
     <>
-    <Helmet>
-    <meta charSet="utf-8" />
-    <title>My QR Reader</title>
-    <meta name="description" content="QR Reader Code Challenge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-  </Helmet>
-    <div className='main'>
-      <div>
-      <img src={image} alt="qr-code" />
-      </div>
-      <section>
-       <div> 
-       <p className='mainbodyText'>Coding daily will help to improve your front-end skills</p>
-       </div>
-       <div>
-        <p className='subbodyText'>Scan the QR code to <br/> and start a coding challenge</p>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My QR Reader</title>
+        <meta name="description" content="QR Reader Code Challenge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
+      <div className="main">
+        <div>
+          <img src={image} alt="qr-code" />
         </div>
-      </section>
-    </div>
+        <section>
+          <div>
+            <p className="mainbodyText">
+              Coding daily will help to improve your front-end skills
+            </p>
+          </div>
+          <div>
+            <p className="subbodyText">
+              Scan the QR code to <br /> and start a coding challenge
+            </p>
+          </div>
+        </section>
+      </div>
     </>
-);
+  );
 }
 
 export default App;
